c54403c2599dce222f6bc38a5452e4e1f6772af5
David Ascencio <lascencio753@gmail.com>
2017-09-13 15:40:31 -0500

    Tarea02

---
+++ AscencioCuba_D_T6LM_Tarea02/EclipseLink/src/pe/edu/cibertec/proyemp/tests/Prueba.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.proyemp.tests;
+
+import java.util.Date;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date);
+	}
+
+}
